dfb8b6fb9b064877303a55175ada8a3aa84ae596
Brooklyn64square <brooklyn64square@github.com>
2025-11-08 16:10:36 +0000

    Fix: Make Autopilot logo much larger and more prominent

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
         <div className="flex justify-between items-center h-24">
           {/* Logo */}
           <Link href="/" className="flex items-center">
-            <div className="relative w-[200px] h-[50px]">
+            <div className="relative w-[320px] h-[80px]">
               <Image
                 src="/assets/logo.png"
                 alt="Autopilot"
